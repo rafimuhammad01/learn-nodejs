7815511813583838f1c384810e4575cc20576aec
rafimuhammad01 <rafi10muhammad@gmail.com>
2021-01-10 00:24:43 +0700

    MongoDB connect + GET request

--- to-do-app/api/routes/todo.js
+++ to-do-app/api/routes/todo.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const router = express.Router();
+const ToDo = require("../models/todo")
 
 router.get("/", (req, res, next) => {
-    
-
-    res.status(200).json({
-        message: "GET Request to /to-do"
+    ToDo.find()
+    .then(docs => {
+        res.status(200).json({
+            message: "GET Request to /to-do",
+            data : docs
+        })
     })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            message: "Error",
+            status : 500,
+            error : err,
+        })
+    })
+    
 })
 
 router.post("/", (req, res, next) => {
--- to-do-app/app.js
+++ to-do-app/app.js
@@ -2,9 +2,17 @@ const express = require("express");
 const app = express();
 const morgan = require('morgan');
 const bodyParser  = require('body-parser');
+const mongoose = require("mongoose");
 
 
 const todoRoutes = require("./api/routes/todo");
+
+mongoose.connect('mongodb+srv://rafi_muhammad01:' + process.env.MONGO_ATLAS_PW + '@cluster0.dumg5.mongodb.net/to-do-api?retryWrites=true&w=majority',
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false}));
