7bb3348c419ef87c49c2409a0c77caba9b693695
rafimuhammad01 <rafi10muhammad@gmail.com>
2021-01-13 10:01:01 +0700

    Policy Update

--- to-do-app/app.js
+++ to-do-app/app.js
@@ -19,15 +19,10 @@ app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*"),
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization" 
-    );
-    if (req.method == "OPTIONS") {
-        res.header("Access-Control-Allow-Headers","PUT, POST, PATCH, DELETE, GET");
-        return res.status(200).json({})
-    }
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PATCH, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  
     next();
 })
 
